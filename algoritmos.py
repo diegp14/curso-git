1b987aa6856f472d58a6acbd0727e8dff0d7db18
Diegp <diegp2001@gmail.com>
2023-06-20 15:49:07 -0600

    Este es mi commit

---
+++ algoritmos.py
@@ -0,0 +1,18 @@
+#BUSQUEDA BINARIA
+def busqueda_binaria(lista, elemento):
+    inicio = 0
+    fin = len(lista) - 1  # la ultima posicion de la lista
+    while inicio <= fin:
+        medio = (inicio + fin)//2
+        valor_medio = lista[medio]
+
+        if valor_medio == elemento:
+            return valor_medio
+        elif valor_medio < elemento:
+            inicio = medio + 1
+        else:
+            fin = medio - 1
+    return None
+
+
+
