1627b94bb82f0b2ca95ff32e61c3811e02874089
Diegp <diegp2001@gmail.com>
2023-08-19 12:51:55 -0600

    commit 19-agosto-23

--- algoritmos.py
+++ algoritmos.py
@@ -16,3 +16,13 @@ def busqueda_binaria(lista, elemento):
 
 
 mi_lista = [1,2,3,4,5,6,7]
+
+
+def my_split(cadena, separador):
+    #print(cadena)
+    my_list = {}
+    for c in cadena:
+        
+        if c == ",":
+            print("Entre")
+
