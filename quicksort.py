2c0a97354fe2b9dd84ab5484fdeabc6d33df9c73
Diegp <diegp2001@gmail.com>
2023-08-19 13:42:32 -0600

    commit al quickshort

--- quicksort.py
+++ quicksort.py
@@ -1,2 +1,8 @@
 print("Segundo archivo!!")
 print("Segundo cambio!!")
+
+#consultar los commits con 
+#git log y/o git log --graph
+
+#para crear nuestras propios comandos con git config --global alias.nombre_comando "el_comando"
+#  
